3a394960a52f79b2788c4c30ac6928f3e8a3d92e
ChapsJust <2238424@carrefour.cegepvicto.ca>
2025-12-07 23:00:28 -0500

    fix -v5 FHAOGgGSB IbgSIUG

--- start.js
+++ start.js
@@ -1,8 +1,23 @@
-try {
-  require("./config.js");
-  require("./dist/index.js");
-} catch (e) {
-  console.error("=== CRASH ERROR ===");
+process.on("uncaughtException", (e) => {
+  console.error("=== UNCAUGHT EXCEPTION ===");
   console.error(e);
   process.exit(1);
-}
+});
+
+process.on("unhandledRejection", (e) => {
+  console.error("=== UNHANDLED REJECTION ===");
+  console.error(e);
+  process.exit(1);
+});
+
+console.log("=== STARTING APP ===");
+console.log("NODE_ENV:", process.env.NODE_ENV);
+console.log("MONGODB:", process.env.MONGODB ? "SET" : "NOT SET");
+console.log("JWTSECRET:", process.env.JWTSECRET ? "SET" : "NOT SET");
+console.log("PORT:", process.env.PORT);
+
+require("./config.js");
+console.log("=== CONFIG LOADED ===");
+
+require("./dist/index.js");
+console.log("=== INDEX LOADED ===");
